e0ccc46f40ae1043932c36e79039a84c2b67faf1
ozgeesert <ozgeesert@gmail.com>
2025-05-09 15:36:43 +0300

    I refreshed all repo

--- src/users/dto/update-user.dto.ts
+++
@@ -1,8 +0,0 @@
-// This class extends the CreateUserDto using PartialType,
-// which makes all properties optional. It is useful for update operations
-// (e.g., PATCH or PUT requests), where not all fields are required.
-import { CreateUserDto } from "./create-user.dto";
-import { PartialType } from '@nestjs/mapped-types';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
-
